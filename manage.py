7517151f7d3acc123fdc248774399dcbe7053d0b
Romario Villalobos <romarioavr@gmail.com>
2024-02-12 21:45:47 -0600

    fin

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import _tkinter
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tesis.settings')
