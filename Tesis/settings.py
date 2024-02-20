71ccf648790ff05f017201d33522b228885a7477
Romario Villalobos <romarioavr@gmail.com>
2024-02-20 17:39:33 -0600

    final

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'Tesis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'TesisApp', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
