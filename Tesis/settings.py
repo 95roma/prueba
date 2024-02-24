58afd0f64b110cd27784e9bc873701af01ec478b
Romario Villalobos <romarioavr@gmail.com>
2024-02-23 19:56:51 -0600

    final ookm

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,7 +79,7 @@ ROOT_URLCONF = 'Tesis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [BASE_DIR / "templates"],
+         "DIRS": [os.path.join(BASE_DIR,  'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
